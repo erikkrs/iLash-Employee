5ad38a732d80d6d5aff7eeaac6f6730863b2a5f0
erikkrs <er1k213.es@gmail.com>
2017-11-13 23:21:34 -0600

    Works well
    submitted to itunes connect and is working well

--- Uber App For Driver/AuthProvider.swift
+++ Uber App For Driver/AuthProvider.swift
@@ -32,7 +32,7 @@ class AuthProvider {
         FIRAuth.auth()?.signIn(withEmail: withEmail, password: password, completion: { (user, error) in
             
             if error != nil {
-                self.handleErrors(err: error as! NSError, loginHandler: loginHandler);
+                self.handleErrors(err: error! as NSError, loginHandler: loginHandler);
             } else {
                 loginHandler?(nil);
             }
@@ -46,7 +46,7 @@ class AuthProvider {
         FIRAuth.auth()?.createUser(withEmail: withEmail, password: password, completion: { (user, error) in
             
             if error != nil {
-                self.handleErrors(err: error as! NSError, loginHandler: loginHandler);
+                self.handleErrors(err: error! as NSError, loginHandler: loginHandler);
             } else {
                 
                 if user?.uid != nil {
